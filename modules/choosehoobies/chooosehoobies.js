1d9c8d4ee61cd5fb79bb6aca3d2c592ae239bd86
Andy <GeekerAndy@outlook.com>
2018-04-24 08:57:49 +0800

    delete redundant files.

--- modules/choosehoobies/chooosehoobies.js
+++
@@ -1,6 +0,0 @@
-/*
- * 处理登录界面跳转提交逻辑
- */
-function jumpToHome() {
-    window.open("../main/main.html");
-}
--- modules/login/login.js
+++
@@ -1,7 +0,0 @@
-/*
- * 处理登录界面跳转提交逻辑
- */
-function jumpToRegister() {
-    plus.webview.open('/modules/register/register.html', 'new', {}, 'slide-in-right', 200);
-
-}
